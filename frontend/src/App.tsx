2478d30091d23a2c8802471f7321e5a83923a653
Farit <faritahmetshin@gmail.com>
2023-01-20 15:19:29 +0300

    Save1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,114 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Navigation from './ui/Navigation';
-import Modal from './Modal/Modal';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+type Data = [Question[]];
+
+type Question = {
+  id: number;
+  question: string;
+  answer: string;
+  id_theme: number;
+  price: number;
+  name_theme: string;
+};
 
 function App(): JSX.Element {
-  const [modalActive, setModalActive] = useState(false);
+  const [questions, setQuestions] = React.useState<Data>([[]]);
+  const [open, setOpen] = React.useState(false);
+  const [current, setCurrent] = React.useState('');
+
+  useEffect(() => {
+    async function questionsData(): Promise<void> {
+      const url = `/api/themeAndQuestions`;
+      const response = await fetch(url);
+      const data: Data = await response.json();
+
+      setQuestions(data);
+    }
+
+    questionsData();
+  }, []);
+
+  function check(answer) {}
+  const handleOpen = (question: any): void => {
+    setCurrent(question);
+    setOpen(true);
+  };
+  const handleClose = (): void => setOpen(false);
+
   return (
-    <BrowserRouter>
-      <div>jopa</div>
+    <>
+      {' '}
+      <div className="game_container">
+        <div className="titles">
+          <div> Кино по смайликам</div>
+          <div> Да/Нет</div>
+          <div> Школьная программа</div>
+          <div> Литературный -ЛОХ-</div>
+          <div>Одна буква</div>
+        </div>
 
-      <div className="open-btn" onClick={(): void => setModalActive(true)}>
-        Кто был на луне
+        <div className="questions">
+          {questions.map((el) => {
+            return el.map((question) => {
+              return (
+                <>
+                  <div
+                    className="btn-score"
+                    onClick={(): void => handleOpen(question)}
+                  >
+                    {question.price}
+                  </div>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style} className="aaa">
+                      <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                      >
+                        {current.name_theme} за {current.price}
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        {current.question}
+                        <div>
+                          <input></input>
+                          <Button onClick={(): void => check(current.answer)}>
+                            {' '}
+                            ответить
+                          </Button>
+                        </div>
+                      </Typography>
+                    </Box>
+                  </Modal>
+                </>
+              );
+            });
+          })}
+        </div>
       </div>
-      <Modal active={modalActive} setActive={setModalActive}>
-        <input placeholder="jjjjjjj" />
-      </Modal>
-      <Modal active={modalActive} setActive={setModalActive}>
-        <input placeholder="444444444444" />
-      </Modal>
-
-      <Navigation />
-      <Routes>
-        {/* <Route path="/" element={<HomePage />} />
-        <Route path="/info" element={<InfoPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/cocktails" element={<CocktailsPage />} />
-        <Route path="/cocktails/:id" element={<CocktailPage />} /> */}
-      </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
--- frontend/src/Modal/Modal.tsx
+++ frontend/src/Modal/Modal.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import './modal.css';
+// import React from 'react';
+// import './modal.css';
 
-const Modal = ({ active, setActive, children }): JSX.Element => {
-  return (
-    <div
-      className={active ? 'modal active' : 'modal'}
-      onClick={(): void => setActive(false)}
-    >
-      <div
-        className={active ? 'modal_content active' : 'modal_content'}
-        onClick={(e): void => e.stopPropagation()}
-      >
-        {children}
-      </div>
-    </div>
-  );
-};
+// const Modal = ({ active, setActive, children }): JSX.Element => {
+//   return (
+//     <div
+//       className={active ? 'modal active' : 'modal'}
+//       onClick={(): void => setActive(false)}
+//     >
+//       <div
+//         className={active ? 'modal_content active' : 'modal_content'}
+//         onClick={(e): void => e.stopPropagation()}
+//       >
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Modal;
+// export default Modal;
